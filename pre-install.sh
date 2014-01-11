9537d8bd6a326a2798ef81af1e601452b4d62d49
devotg <devotg.com@gmail.com>
2014-01-12 03:48:06 +0800

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -1,3 +1,6 @@
 hostname -f
-yum -y update
 ln -sf /usr/share/zoneinfo/Asia/Chongqing /etc/localtime
+
+yum -y update
+rpm --import http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
+rpm -Kih http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
