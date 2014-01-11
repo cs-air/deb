89ad14d090e83a6c6b871807423aff9eb49ab471
devotg <devotg.com@gmail.com>
2014-01-11 23:24:16 +0800

    Create virtualmin-with-chruby-installer.sh

---
+++ virtualmin-with-chruby-installer.sh
@@ -0,0 +1,30 @@
+cd ~
+wget -O virtualmin-install.sh http://software.virtualmin.com/gpl/scripts/install.sh
+sh virtualmin-install.sh
+ 
+wget -O chruby-0.3.8.tar.gz https://github.com/postmodern/chruby/archive/v0.3.8.tar.gz
+tar -xzvf chruby-0.3.8.tar.gz
+cd chruby-0.3.8/
+sudo make install
+cd ~
+ 
+wget -O ruby-install-0.3.4.tar.gz https://github.com/postmodern/ruby-install/archive/v0.3.4.tar.gz
+tar -xzvf ruby-install-0.3.4.tar.gz
+cd ruby-install-0.3.4/
+sudo make install
+cd ~
+ 
+cat <<EOF > /etc/profile.d/chruby.sh
+[ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ] || return
+ 
+source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
+chruby ruby
+function rbv()
+{
+  case "$1" in
+    "") chruby;;
+    *) chruby *;;
+  esac
+}
+EOF
