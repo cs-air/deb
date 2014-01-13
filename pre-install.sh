991c806393fab2c0d35a3a916776b40262b44097
devotg <devotg.com@gmail.com>
2014-01-13 23:04:58 +0800

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -6,5 +6,5 @@ yum -y install gcc-c++ curl-devel httpd-devel
 
 #rpm --import http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
 #rpm -Kih http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-yum install http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+yum -y install http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 yum -y install libyaml-devel
