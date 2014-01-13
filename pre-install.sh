7815c5718e6db35acbbeb89724aad997ec7259c8
devotg <devotg.com@gmail.com>
2014-01-13 22:10:19 +0800

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -1,7 +1,10 @@
 hostname -f
 ln -sf /usr/share/zoneinfo/Asia/Chongqing /etc/localtime
 
+yum -y install gcc-c++ curl-devel httpd-devel
 yum -y update
-rpm --import http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
-rpm -Kih http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+
+#rpm --import http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
+#rpm -Kih http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 yum install http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+yum -y install libyaml-devel
