239286c2131ff2e5565a87f0102e1c7f42008906
devotg <devotg.com@gmail.com>
2014-02-08 14:55:43 +0800

    Update pptp-mini.sh

--- pptp-mini.sh
+++ pptp-mini.sh
@@ -3,7 +3,7 @@
 #cat /dev/tun
 echo "
 PPTP install script for an OpenVZ VPS, Tested on Debian 7
-wget https://raw.github.com/devotg/dev-deb/master/pptp.sh && sh pptp.sh
+wget https://raw.github.com/devotg/dev-deb/master/pptp-mini.sh && sh pptp-mini.sh
 "
 
 echo "######################################################"
@@ -25,8 +25,8 @@ apt-get install pptpd -y
 #apt-get install -y iptables logrotate tar cpio perl
 
 # config
-#rm -r /dev/ppp
-#mknod /dev/ppp c 108 0
+rm -rf /dev/ppp
+mknod /dev/ppp c 108 0
 #echo 1 > /proc/sys/net/ipv4/ip_forward 
 #echo "mknod /dev/ppp c 108 0" >> /etc/rc.local
 #echo "echo 1 > /proc/sys/net/ipv4/ip_forward" >> /etc/rc.local
@@ -39,9 +39,6 @@ END
 cat >> /etc/ppp/pptpd-options <<END
 ms-dns 8.8.8.8
 ms-dns 8.8.4.4
-noipx
-mtu 1490
-mru 1490
 END
 
 cat >> /etc/ppp/chap-secrets <<END
@@ -62,9 +59,6 @@ cat > /etc/network/if-pre-up.d/iptables <<END
 iptables-restore < /etc/iptables.conf
 END
 chmod +x /etc/network/if-pre-up.d/iptables
-cat > /etc/ppp/ip-up.d/pptpd <<END
-ifconfig $1 mtu 1490
-END
 
 sleep 5
 /etc/init.d/pptpd restart
