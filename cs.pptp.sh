e7d6df36d38b7e92549241272bf7c67568cf817b
devotg <devotg.com@gmail.com>
2015-01-30 17:47:16 +0800

    Update cs.pptp.sh

--- cs.pptp.sh
+++ cs.pptp.sh
@@ -7,16 +7,8 @@ wget https://raw.github.com/devotg/dev-deb/master/cs.pptp.sh && sh cs.pptp.sh
 echo "######################################################"
 echo "Enter username:" && read u
 echo "Enter password:" && read p
-echo "Enter ip:" && read ip
 
-# install
 apt-get update
-apt-get purge pptpd ppp bcrelay -y
-rm -rf /etc/pptpd.conf
-rm -rf /etc/ppp
-rm -rf /etc/sysctl.d/pptpd
-rm -rf /etc/iptables.conf
-rm -rf /etc/network/if-pre-up.d/iptables
 apt-get install pptpd -y
 
 cat >> /etc/pptpd.conf <<END
@@ -27,36 +19,23 @@ END
 cat >> /etc/ppp/pptpd-options <<END
 ms-dns 8.8.8.8
 ms-dns 8.8.4.4
-noipx
 END
 
 cat >> /etc/ppp/chap-secrets <<END
 $u * $p *
 END
 
+service pptpd restart
+
 cat > /etc/sysctl.d/pptpd <<END
 net.ipv4.ip_forward=1
 END
 sysctl -p
 
-iptables -t nat -A POSTROUTING -j SNAT --to-source $ip
-iptables-save > /etc/iptables.conf
-
-cat > /etc/network/if-pre-up.d/iptables <<END
-#!/bin/sh
-iptables-restore < /etc/iptables.conf
-END
-chmod +x /etc/network/if-pre-up.d/iptables
-cat > /etc/ppp/ip-up.d/pptpd <<END
-ifconfig $1 mtu 1490
-END
-
-sleep 5
-/etc/init.d/pptpd restart
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE && iptables-save
 
 echo
 echo "######################################################"
 echo "PPTP setup complete!"
-echo "Connect to your VPS at $ip with these credentials:"
 echo "Username:$u ##### Password: $p"
 echo "######################################################"
