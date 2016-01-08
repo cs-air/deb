ac74715041fd6c1e8fb3bd81be0dcbab63858bba
cs-air <cs-air@users.noreply.github.com>
2016-01-08 16:51:36 +0800

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,3 +1,6 @@
-sudo apt-get install -y python build-essential
-sudo curl -sL https://deb.nodesource.com/setup_5.x | bash -
-sudo apt-get install -y nodejs
+apt-get install -y python build-essential
+curl -sL https://deb.nodesource.com/setup_5.x | bash -
+apt-get install -y nodejs
+
+npm install -g ionic@beta
+npm cache clean
