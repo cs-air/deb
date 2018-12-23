3ab9e5bb19aa99f506431d054dc625806dc3f244
cs-id <cs-id@users.noreply.github.com>
2018-12-23 20:05:19 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -10,3 +10,18 @@ apt-get update
 apt-get install -y docker-ce
 #apt-cache madison docker-ce
 #apt-get install docker-ce=<VERSION_STRING>
+
+
+docker pull hwdsl2/ipsec-vpn-server
+wget -O vpn.env https://github.com/hwdsl2/docker-ipsec-vpn-server/blob/master/vpn.env.example
+#edit vpn.env
+docker run \
+    --name vpn \
+    --env-file ./vpn.env \
+    --restart=always \
+    -p 500:500/udp \
+    -p 4500:4500/udp \
+    -v /lib/modules:/lib/modules:ro \
+    -d --privileged \
+    hwdsl2/ipsec-vpn-server
+    
