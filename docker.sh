5f6c48f22553ea65f49dbb5e61c20cf6c9df0a1c
cs-id <cs-id@users.noreply.github.com>
2018-12-23 19:47:02 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,12 +1,12 @@
 apt-get remove docker docker-engine docker.io
 uname -r
 apt-get update
-apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 #9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get update
-apt-get install docker-ce
+apt-get install -y docker-ce
 #apt-cache madison docker-ce
 #apt-get install docker-ce=<VERSION_STRING>
